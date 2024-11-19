a3164c4d2a22c9cfc88a22dbdb2186e05b305860
adhidhyaRP <153973098+adhidhyaRP@users.noreply.github.com>
2024-11-19 13:25:10 +0530

    Update TemperatureControl.jsx

--- src/TemperatureControl.jsx
+++ src/TemperatureControl.jsx
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import L from 'leaflet';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import './TemperatureControl.css';
 import './MaintenanceAlert.css';
 import 'leaflet/dist/leaflet.css';
 
-// Fix marker icon issue
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconUrl: markerIcon,
-  shadowUrl: markerShadow,
+// Custom marker icon using URL
+const customMarkerIcon = new L.Icon({
+  iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
 });
 
 const destinations = {
@@ -52,12 +53,12 @@ function TemperatureControl() {
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
           {/* Marker for Current Location */}
-          <Marker position={currentLocation}>
+          <Marker position={currentLocation} icon={customMarkerIcon}>
             <Popup>Current Location (Truck)</Popup>
           </Marker>
 
           {/* Marker for Destination Location */}
-          <Marker position={{ lat: destination.lat, lng: destination.lng }}>
+          <Marker position={{ lat: destination.lat, lng: destination.lng }} icon={customMarkerIcon}>
             <Popup>Destination: {destination.name}</Popup>
           </Marker>
 
